8a4927121e130ecf39bdac2e185eaada7f8ca5b7
ClementMONDARY <clementmondary77173@gmail.com>
2023-12-06 16:04:41 +0100

    barre de recherche complete

--- LoL_API/script.js
+++ LoL_API/script.js
@@ -1,35 +1,11 @@
-const selectionSection = document.getElementById("selection");
-const jsonUrl =
-  "https://ddragon.leagueoflegends.com/cdn/13.23.1/data/fr_FR/champion.json";
+document.addEventListener("DOMContentLoaded", async function () {
+  const selectionSection = document.getElementById("selection");
+  const jsonUrl =
+    "https://ddragon.leagueoflegends.com/cdn/13.23.1/data/fr_FR/champion.json";
 
-const displayRandomChampionInfo = (championData) => {
-  const championNames = Object.keys(championData.data);
-  const randomChampionName =
-    championNames[Math.floor(Math.random() * championNames.length)];
-  const randomChampion = championData.data[randomChampionName];
+  const searchInput = document.querySelector("form input");
+  searchInput.addEventListener("input", handleSearchInput);
 
-  const h2Element = document.querySelector(".figc h2");
-  const h1Element = document.querySelector(".figc h1");
-  const imgElement = document.querySelector(".front img");
-  const aboutChampionLink = document.getElementById("aboutChampion");
-
-  console.log("Nom du champion aléatoire :", randomChampion.name);
-  console.log("Titre du champion aléatoire :", randomChampion.title);
-  console.log(
-    "Lien sprite du champion aléatoire :",
-    `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${randomChampion.id}_0.jpg`
-  );
-
-  // Ajouter le lien vers champion.html avec l'ID du champion aléatoire
-  aboutChampionLink.href = `champion.html?id=${randomChampion.id}`;
-
-  h2Element.textContent = randomChampion.title;
-  h1Element.textContent = randomChampion.name;
-  const championImageLink = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${randomChampion.id}_0.jpg`;
-  imgElement.src = championImageLink;
-};
-
-async function main() {
   async function fetchChampionData() {
     try {
       const response = await fetch(jsonUrl);
@@ -56,13 +32,13 @@ async function main() {
     const championImageLink = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`;
 
     const championHTML = `
-    <a href="champion.html?id=${champion.id}">
-      <div>
-        <img src="${championImageLink}" />
-        <h3>${champion.name}</h3>
-      </div>
-    </a>
-  `;
+      <a href="champion.html?id=${champion.id}">
+        <div>
+          <img src="${championImageLink}" />
+          <h3>${champion.name}</h3>
+        </div>
+      </a>
+    `;
 
     selectionSection.insertAdjacentHTML("beforeend", championHTML);
   }
@@ -74,7 +50,6 @@ async function main() {
     });
   });
 
-  championData = await fetchChampionData();
   displayRandomChampionInfo(championData);
   updateChampionInfoInHTML(championData);
 
@@ -84,29 +59,62 @@ async function main() {
     updateChampionInfoInHTML(updatedChampionData);
     localStorage.setItem("championData", JSON.stringify(updatedChampionData));
   });
-}
 
-function updateChampionInfoInHTML(championData) {
-  selectionSection.innerHTML = "";
+  function handleSearchInput(event) {
+    const searchTerm = event.target.value.toLowerCase();
+    const championLinks = selectionSection.querySelectorAll("a");
 
-  const championNames = Object.keys(championData.data);
+    championLinks.forEach((link) => {
+      const championName = link.querySelector("h3").textContent.toLowerCase();
 
-  championNames.forEach((championName) => {
-    const champion = championData.data[championName];
+      if (championName.includes(searchTerm)) {
+        link.style.display = "block";
+      } else {
+        link.style.display = "none";
+      }
+    });
+  }
 
-    const championImageLink = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`;
+  function updateChampionInfoInHTML(championData) {
+    selectionSection.innerHTML = "";
 
-    const championHTML = `
-    <a href="champion.html?id=${champion.id}">
-      <div>
-        <img src="${championImageLink}" />
-        <h3>${champion.name}</h3>
-      </div>
-    </a>
-  `;
+    const championNames = Object.keys(championData.data);
 
-    selectionSection.insertAdjacentHTML("beforeend", championHTML);
-  });
-}
+    championNames.forEach((championName) => {
+      const champion = championData.data[championName];
 
-main();
+      const championImageLink = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`;
+
+      const championHTML = `
+        <a href="champion.html?id=${champion.id}">
+          <div>
+            <img src="${championImageLink}" />
+            <h3>${champion.name}</h3>
+          </div>
+        </a>
+      `;
+
+      selectionSection.insertAdjacentHTML("beforeend", championHTML);
+    });
+  }
+
+  function displayRandomChampionInfo(championData) {
+    const championNames = Object.keys(championData.data);
+    const randomChampionName =
+      championNames[Math.floor(Math.random() * championNames.length)];
+    const randomChampion = championData.data[randomChampionName];
+
+    const h2Element = document.querySelector(".figc h2");
+    const h1Element = document.querySelector(".figc h1");
+    const imgElement = document.querySelector(".front img");
+    const aboutChampionLink = document.getElementById("aboutChampion");
+
+    // Ajouter le lien vers champion.html avec l'ID du champion aléatoire
+    aboutChampionLink.href = `champion.html?id=${randomChampion.id}`;
+
+    h2Element.textContent = randomChampion.title;
+    h1Element.textContent = randomChampion.name;
+    const championImageLink = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${randomChampion.id}_0.jpg`;
+    imgElement.src = championImageLink;
+  }
+});
